10d254025db732917fbd886b2c994d0a23c5df39
identisphere <niliproject123@gmail.com>
2016-06-24 13:00:30 +0300

    BT string message structure

--- app/src/main/java/com/nili/globals/Globals.java
+++ app/src/main/java/com/nili/globals/Globals.java
@@ -14,6 +14,16 @@ final public class Globals
 		static public int WAITING_FOR_USER_LIFT = 1;
 	}
 
-
+	////////////////////////////////////////
+	// STRUCTURE OF BT STRING
+	/////////////////////////////////////
+	// 4th fret --- 1st fret
+	// 1st digit in fret: top string (closest to lead strip. LED #1)
+	// 6st digit in fret: bottom string (farthest from lead strip. LED #6)
+	// example: "111111000000100000000001"
+	//  4th fret: full lights
+	//	3rd fret: no light
+	//	2nd fret: top light on
+	//	1st fret: bottom light on
 }
 
